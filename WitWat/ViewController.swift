d388f9bf5df1d013a3d938322058ca22cffb2fa0
Nico Cvitak <ncvitak@gmail.com>
2015-10-18 14:08:00 -0400

    Initial Commit

---
+++ WitWat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WitWat
+//
+//  Created by Nico Cvitak on 2015-10-18.
+//  Copyright © 2015 UW-AppDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
